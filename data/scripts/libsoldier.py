89edd23e2fa2d4625188ce8b912908208ce4d018
Joel Gomes da Silva <joelgomes1994@hotmail.com>
2021-02-04 12:32:17 -0300

    Adicionada lógica básica ao soldado/inimigo, armas e funcionalidade de o soldado e o inimigo compartilharem a mesma lógica, ativando um ou outro através da propriedade Enemy do objeto SoldierCollision.

---
+++ data/scripts/libsoldier.py
@@ -0,0 +1,62 @@
+import bge
+
+from scripts import bgf
+from mathutils import Vector
+
+TRACK_TIME = 15
+MOVE_SPEED = 0.25
+
+ANIMS = {
+	"Idle" : (0, 63, bge.logic.KX_ACTION_MODE_LOOP),
+	"Run" : (70, 85, bge.logic.KX_ACTION_MODE_LOOP),
+	"Fire" : (90, 96, bge.logic.KX_ACTION_MODE_PLAY),
+	"Death" : (100, 152, bge.logic.KX_ACTION_MODE_PLAY),
+}
+
+def runEnemy(cont):
+	own = cont.owner
+	always = cont.sensors["Always"]
+	
+	if always.status == bge.logic.KX_INPUT_JUST_ACTIVATED:
+		own.childrenRecursive["Soldier"].replaceMesh("Soldier2")
+		own.childrenRecursive["SoldierGun"].replaceMesh("AK47")
+		
+	processAnimation(cont)
+	processTrack(cont)
+
+def runAlly(cont):
+	own = cont.owner
+	always = cont.sensors["Always"]
+	
+	if always.status == bge.logic.KX_INPUT_JUST_ACTIVATED:
+		pass
+	
+	processAnimation(cont)
+	processTrack(cont)
+	
+def processTrack(cont):
+	own = cont.owner
+	track = cont.actuators["TrackArmature"]
+	track.object = own.childrenRecursive["SoldierTarget"]
+	cont.activate(track)
+
+def processAnimation(cont):
+	own = cont.owner
+	armature = own.childrenRecursive["SoldierArmature"]
+	action = ANIMS[own["Action"]]
+	armature.playAction("Soldier", action[0], action[1], play_mode=action[2])
+
+def runSoldier(cont):
+	always = cont.sensors["Always"]
+	
+	if cont.owner.groupObject is None:
+		cont.owner.endObject()
+		return
+	
+	if always.positive:
+		
+		if cont.owner["Enemy"]:
+			runEnemy(cont)
+			
+		else:
+			runAlly(cont)
