4d5a8a90502e32d123ecd584c40e6238e60552cb
Joel Gomes da Silva <joelgomes1994@hotmail.com>
2021-02-07 14:04:45 -0300

    Implementado som e sangue quando o soldado é acertado por tiro e som.

---
+++ data/scripts/libbullet.py
@@ -0,0 +1,27 @@
+import bge
+
+from scripts import bgf
+from mathutils import Vector
+from random import random
+
+DAMAGE_DEFAULT = 25
+
+def runBullet(cont):
+	own = cont.owner
+	collision = cont.sensors["Collision"]
+	
+	if collision.positive:
+		
+		if not "Damage" in own:
+			own["Damage"] = DAMAGE_DEFAULT
+		
+		if "Life" in collision.hitObject:
+			collision.hitObject["Life"] -= own["Damage"]
+			blood = own.scene.addObject("BloodHit", own, 120)
+			blood.localPosition.y -= 3
+			blood.localScale *= 2
+			blood.alignAxisToVect(blood.getVectTo(own.scene.active_camera.worldPosition)[1], 2)
+			sound = bgf.playSfx("ShotHit", buffer=True, is3D=True, refObj=collision.hitObject, distMax=150)
+			sound.pitch = 1 + (random() * 0.5 - 0.25)
+			
+		own.endObject()
